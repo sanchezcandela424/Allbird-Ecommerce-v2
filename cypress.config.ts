18f42ca172131a7539527ab0a955ed58ba663189
Cande Sanchez <sanchezcandela424@gmail.com>
2025-11-25 01:13:35 -0600

    fix: remove invalid retryOnStatusCodeFailure from cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
     trashAssetsBeforeRuns: true,
     chromeWebSecurity: false,
     modifyObstructiveCode: true,
-    retryOnStatusCodeFailure: false,
     setupNodeEvents(on, config) {
       on('task', {
         log(message: string) {
